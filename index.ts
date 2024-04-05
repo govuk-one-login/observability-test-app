9155ad452d5f0e180cfa17483e1b774b4e23e8b4
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2024-04-05 11:36:38 +0100

    Add healthcheck

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/healthcheck', (req, res) => {
+  res.send('ok');
+});
+
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
